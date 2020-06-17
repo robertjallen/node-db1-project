27e205decc586d7003eb569815220e154e434a87
robertjallen <bobbidigi34@gmail.com>
2020-06-17 18:30:12 -0400

    add create new account

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -2,6 +2,30 @@ const express = require('express');
 const router = express.Router();
 const db = require("../data/dbConfig.js");
 
+
+//====================
+//      CREATE
+//====================
+router.post("/", async (req, res, next) => {
+  try {
+    const payload = {
+      name: req.body.name,
+      budget: req.body.budget
+    }
+    const [newAccount] = await db.insert(payload).into("accounts")
+		// calling `.first()` is doing the same thing as `.limit(1)` and destructuring the result
+    const message = await db.first("*").from("accounts").where("id", newAccount)
+    res.status(201).json(message)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+
+//====================
+//      READ
+//====================
 router.get("/", async (req, res, next) => {
 	try {
 		// translates to `SELECT * FROM "accounts";`
